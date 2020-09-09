cf4939a8c5c256e4c2f9a853a6d7d3fdff847e6f
Anton Yaroshchuk <61189090+Nagashi-A-A@users.noreply.github.com>
2020-09-09 15:12:24 +0300

    Add files via upload

---
+++ ITMO.G124.ASP.NET.Yaroshchuk.Lab7/RespRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.G124.ASP.NET.Yaroshchuk.Lab1
+{
+    public class RespRepo
+    {
+        private static RespRepo repository = new RespRepo();
+        private List<GuestResponse> responses = new List<GuestResponse>();
+        public static RespRepo GetRepository()
+        {
+            return repository;
+        }
+        public IEnumerable<GuestResponse> GetAllResponses()
+        {
+            return responses;
+        }
+        public void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
